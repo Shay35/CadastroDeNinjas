329952adeaefaaabcdef89534429c29cec264417
Shay35 <shayro56@gmail.com>
2025-04-07 10:34:57 -0300

    Modelagem do banco de dados, NinjaModel e MissoesModel concluídos

---
+++ src/main/java/dev/java/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,23 @@
+package dev.java.CadastroDeNinjas.Missoes;
+
+import dev.java.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_missoes")
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String dificuldade;
+
+    //Uma missao pode ter varios ninjas
+    @OneToMany(mappedBy = "missoes")
+    private List<NinjaModel> ninjas;
+}
--- src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,6 +1,8 @@
-package dev.java.CadastroDeNinjas;
+package dev.java.CadastroDeNinjas.Ninjas;
 
+import dev.java.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
@@ -9,9 +11,18 @@ public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
     private int idade;
+
+    //@ManyToOne - um ninja tem uma unica missao.
+    //O joinColumn gera uma terceira tabela que une a tabela de ninjas e a tabela de missoes
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") //Foreing key ou chave estrangeira
+    private MissoesModel missoes;
 
     public NinjaModel() {
     }
@@ -20,6 +31,7 @@ public class NinjaModel {
         this.nome = nome;
         this.email = email;
         this.idade = idade;
+        this.missoes = missoes;
     }
 
     public String getNome() {
