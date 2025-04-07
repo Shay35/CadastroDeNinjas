35b33e36adaa81e82b1fec46a90c7fb89376be34
Shay35 <shayro56@gmail.com>
2025-04-07 12:26:33 -0300

    fix: Adiciona o @Column na minha class

--- src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -6,8 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Entity
 @Table(name = "tb_cadastro")
 @Data //cria getter e setter
@@ -17,13 +15,19 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "nome")
     private String nome;
 
     @Column(unique = true)
     private String email;
 
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "idade")
     private int idade;
 
     //@ManyToOne - um ninja tem uma unica missao.
