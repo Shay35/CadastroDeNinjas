674b563792c312f2d7ecd3d626de7fad66c8cb21
Shay35 <shayro56@gmail.com>
2025-04-07 17:09:19 -0300

    build: Conecta o banco de dados H2 a minha aplicação

--- src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class NinjaController {
 
-    @GetMapping("/boasvindas")
+    @GetMapping("/boasVindas")
     public String boasVindas() {
         return "Essa é a minha primeira mensagem nessa rota.";
     }
