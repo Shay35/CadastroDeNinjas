f1a1fbc8919347f062294e5137e2d20743c27ac4
Shay35 <shayro56@gmail.com>
2025-04-08 15:14:22 -0300

    feat: Inclui as todos os métodos do Ninja controller

--- src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,15 +1,46 @@
 package dev.java.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
 
-    @GetMapping("/boasVindas")
-    public String boasVindas() {
-        return "Essa é a minha primeira mensagem nessa rota.";
+    @Autowired
+    private NinjaService ninjaService;
+
+    //Adicionar Ninja
+    @PostMapping("/adicionar")
+    public NinjaModel adicionarNinja( @RequestBody NinjaModel ninja) {
+        return ninjaService.adicionarNinja(ninja);
+    }
+
+    //Procurar Ninja por ID
+    @GetMapping("/{id}")
+    //Optional: usado para indicar que o metodo pode ou não retornar um valor
+    public Optional<NinjaModel> buscarNinjaPorId(@PathVariable Long id) {
+        return ninjaService.buscarNinjaPorId(id);
+    }
+
+    //Mostrar todos os ninjas
+    @GetMapping("/mostrar")
+    public List<NinjaModel> mostrarNinjas() {
+        return ninjaService.mostrarNinjas();
+    }
+
+    //Atualizar dados dos Ninjas
+    @PutMapping("/{id}")
+    public NinjaModel atualizarNinja(@PathVariable Long id, @RequestBody NinjaModel ninja) {
+        return ninjaService.atualizarNinja(id, ninja);
+    }
+
+    //Deletar Ninja
+    @DeleteMapping("/{id}")
+    public void deletarNinja(@PathVariable Long id) {
+        ninjaService.deletarNinja(id);
     }
 }
