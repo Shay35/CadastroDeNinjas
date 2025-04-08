093547c1293baac937acb127c4aacedd80d85d9b
Shay35 <shayro56@gmail.com>
2025-04-08 15:09:37 -0300

    feat: Cria os métodos adicionar, mostrar, buscar ninja por id, atualizar e deletar da classe Ninja Service

--- src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -1,4 +1,43 @@
 package dev.java.CadastroDeNinjas.Ninjas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class NinjaService {
+
+    @Autowired
+    private NinjaRepository ninjaRepository;
+
+    public NinjaModel adicionarNinja(NinjaModel ninja) {
+        return ninjaRepository.save(ninja);
+    }
+
+    public Optional<NinjaModel> buscarNinjaPorId(Long id) {
+        return ninjaRepository.findById(id);
+    }
+
+    public List<NinjaModel> mostrarNinjas() {
+        return ninjaRepository.findAll();
+    }
+
+    public NinjaModel atualizarNinja(Long id, NinjaModel atualizado) {
+        if(!ninjaRepository.existsById(id)) {
+            System.out.println("Ninja não encontrado.");
+        }else {
+            atualizado.setId(id);
+        }
+        return ninjaRepository.save(atualizado);
+    }
+
+    public void deletarNinja(Long id) {
+        if(!ninjaRepository.existsById(id)) {
+            System.out.println("Ninja não encontrado.");
+        }else {
+            ninjaRepository.deleteById(id);
+        }
+    }
 }
